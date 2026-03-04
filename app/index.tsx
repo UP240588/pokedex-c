e8fc01116d6f9aa37efc2f7c0f0da757ffe68d0a
UP240588 <up240588@alumnos.upa.edu.mx>
2026-03-04 15:45:32 -0600

    show pokemon image on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
-import { Text, View } from "react-native";
+import PokemonCard from "@/components/PokemonCard";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
 
-export default function Index() {
+  export default function Index() {
 
 
-
+  const [results, setResults] = useState<any[]> ([]);
   useEffect(() => {
     console.log("Entre en pantalla");
     getPokemons();
@@ -12,25 +13,35 @@ export default function Index() {
 
   const getPokemons = async () => {
     try {
-    //const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
-    const URL = "https://pokeapi.co//pokemon?limit=100000&offset=0"
-    const response = await fetch(URL, {  
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    const response = await fetch(URL, {   //fetch jalar informacion
       method: "GET",
     }); 
 
     if (response.ok) {
-      console.log("Request ok");
+      const data = await response.json();  //json convertir a objeto
+      setResults(data.results);
     } else {
-      console.log("Bad request");
+      console.log("Bad request"); 
     }
   } catch (error) {
     console.log("Ocurrió un error");
   }
-};
+}; 
 
   return (
     <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {results.map((item)=>{
+        return (<PokemonCard 
+                  key ={item.name} 
+                  name ={item.name}
+                  url ={item.url}
+                ></PokemonCard>
+               );
+      })}
     </View>
   );
 }
+
+
+//hola prueba
